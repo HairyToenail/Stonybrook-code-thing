e137104d063683ccaeb5002c23d41f04ba9a040b
William Chen <76022647+bobbybotbop@users.noreply.github.com>
2023-07-31 10:08:58 -0400

    fixded

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pyvista as pv
 from pyvista import examples
 
 mesh = pv.read("textured_output.obj")
-mesh = mesh.rotate_x(180, transform_all_input_vectors=True, inplace=True)
+#mesh = mesh.rotate_x(180, transform_all_input_vectors=True, inplace=True)
 
 texture = pv.read_texture("textured_output.jpg")
 # print(type(texture))
@@ -20,7 +20,7 @@ texture = pv.read_texture("textured_output.jpg")
 # # surf = surf.decimate_pro(0.75)  # reduce the density of the mesh by 75%
 
 # Define some helpers - ignore these and use your own data.
-def generate_points(subset=1):#bigger subset more points ohhhh okok
+def generate_points(subset=.1):#bigger subset more points ohhhh okok
     """A helper to make a 3D NumPy array of points (n_points by 3)"""
     dataset = pv.PolyData("textured_output.obj")#this is where it is btw ohhh ok thanks
     ids = np.random.randint(low=0, high=dataset.n_points - 1, size=int(dataset.n_points * subset))
@@ -40,11 +40,11 @@ points[0:5, :]
 
 point_cloud = pv.PolyData(points) #this is where we create a point cloud
 
-data = points[:, -1]
+data = points[:, 1]
 point_cloud["elevation"] = data
-mesh = point_cloud.rotate_x(90, transform_all_input_vectors=True, inplace=True)
+point_cloud = point_cloud.rotate_x(90, transform_all_input_vectors=True, inplace=True)
 
-mesh.plot(eye_dome_lighting=True)
+point_cloud.plot()
 # point_cloud.plot(render_points_as_spheres=True)
 print(len(points))
 #mesh.plot(texture=texture)
